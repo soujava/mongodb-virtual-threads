210288689cc4b6ab4f38a4a043bf67810c5500fa
Otavio Santana <otaviopolianasantana@gmail.com>
2025-02-23 18:36:29 +0000

    feat: initial project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/org/jnosql/demoee/Developer.java
@@ -0,0 +1,35 @@
+package org.jnosql.demoee;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+public record Developer(
+        @Id String id,
+        @Column String name,
+        @Column LocalDate birthday
+) {
+
+    public static Developer newDeveloper(String name, LocalDate birthday) {
+        Objects.requireNonNull(name, "name is required");
+        Objects.requireNonNull(birthday, "birthday is required");
+        return new Developer(
+                UUID.randomUUID().toString(),
+                name,
+                birthday);
+    }
+
+    public Developer update(String name, LocalDate birthday) {
+        Objects.requireNonNull(name, "name is required");
+        Objects.requireNonNull(birthday, "birthday is required");
+        return new Developer(
+                this.id(),
+                name,
+                birthday);
+    }
+}
---
+++ src/main/java/org/jnosql/demoee/DevelopersResource.java
@@ -0,0 +1,67 @@
+package org.jnosql.demoee;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.jnosql.mapping.document.DocumentTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Path("developers")
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
+public class DevelopersResource {
+
+    @Inject
+    DocumentTemplate template;
+
+    @GET
+    public List<Developer> listAll(@QueryParam("name") String name) {
+        if (name == null) {
+            return template.select(Developer.class).result();
+        }
+
+        return template.select(Developer.class)
+                .where("name")
+                .like(name)
+                .result();
+    }
+
+    public record NewDeveloperRequest(String name, LocalDate birthday) {
+    }
+
+    @POST
+    public Developer add(NewDeveloperRequest request) {
+        var newDeveloper = Developer.newDeveloper(request.name(), request.birthday());
+        return template.insert(newDeveloper);
+    }
+
+    @Path("{id}")
+    @GET
+    public Developer get(@PathParam("id") String id) {
+        return template.find(Developer.class, id)
+                .orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
+    }
+
+    public record UpdateDeveloperRequest(String name, LocalDate birthday) {
+    }
+
+    @Path("{id}")
+    @PUT
+    public Developer update(@PathParam("id") String id, UpdateDeveloperRequest request) {
+        var developer = template.find(Developer.class, id)
+                .orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
+        var updatedDeveloper = developer.update(request.name(), request.birthday());
+        return template.update(updatedDeveloper);
+
+    }
+
+    @Path("{id}")
+    @DELETE
+    public void delete(@PathParam("id") String id) {
+        template.delete(Developer.class, id);
+    }
+
+}
